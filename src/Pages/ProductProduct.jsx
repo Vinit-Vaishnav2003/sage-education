62f80b98f2976c43842b1977f85e180939060e98
Vinit <vinitvaishnav2@gmail.com>
2024-07-24 21:03:34 +0530

    product cahnge

--- src/Pages/ProductProduct.jsx
+++ src/Pages/ProductProduct.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './Product.css';
 
 const Product = () => {
-    const images =[
-        { url: '/src/assets/product/CAREER_NAVIGATOR.jpg', alt: 'CAREER NAVIGATOR' },
-        { url: '/src/assets/product/CAREER_FINDER.jpg', alt: 'CAREER_FINDER' },
-        { url: '/src/assets/product/CAREER_REPORT.jpg', alt: 'CAREER_REPORT' }
-    ];
+  const products = [
+    { url: '/src/assets/product/CAREER_NAVIGATOR.jpg', alt: 'CAREER NAVIGATOR', content: 'PERSONALISED - PSYCHOMETRIC CAREER REPORT 10TH GRADE (MAIN)' },
+    { url: '/src/assets/product/CAREER_FINDER.jpg', alt: 'CAREER_FINDER', content: 'PERSONALISED CAREER MAPS 11-12TH GRADE' },
+    { url: '/src/assets/product/CAREER_REPORT.jpg', alt: 'CAREER_REPORT', content: 'INTROSPECTIVE CAREER NAVIGATOR 8-9TH GRADE' }
+  ];
+
   return (
-    <div>
-       <div className="product-image-gallery">
-        {images.map((image, index) => (
+    <div className='product-container'>
+      <div className="product-image-gallery">
+        {products.map((product, index) => (
           <div className="product-image-card" key={index}>
-            <img src={image.url} alt={image.alt} />
+            <img src={product.url} alt={product.alt} />
+            <p>{product.content}</p>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
+
