a816cb38e20313bb6d454570fc1e374e97fe40c6
Vinit <vinitvaishnav2@gmail.com>
2024-07-27 22:17:59 +0530

    correct website

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,11 +4,10 @@ import './About.css'
 
 const About = () => {
   return (
-    <div className="about">
-      <div className="container">
-        <img src={image} />
-        <br />
-        <p>We are a youth-driven, career-education firm run by young people with the goal of providing students with
+    <div className="container about">
+      <img src={image}/>
+      <br/>
+      <p>We are a youth-driven, career-education firm run by young people with the goal of providing students with
           simple, yet psychologically sound, career-counselling services with credible and legit information about
           careers, colleges, subjects, degrees, industries, and everything else matters in the process of career
           decision-making. With personal contact when working with 10th graders, we try to solve their every
@@ -16,7 +15,7 @@ const About = () => {
           obtaining proper career guidance. We have guided 1000+ students till now and are seeking to achieve an
           ultimate goal of reaching and helping every tier 2 and tier 3 city student in their
           journey to pursue their bright career.</p>
-      </div>
+       
     </div>
   )
 }
--- src/Pages/ProductProduct.jsx
+++ src/Pages/ProductProduct.jsx
@@ -9,16 +9,14 @@ const Product = () => {
   ];
 
   return (
-    <div className="product">
-      <div className='product-container'>
-        <div className="product-image-gallery">
-          {products.map((product, index) => (
-            <div className="product-image-card" key={index}>
-              <img src={product.url} alt={product.alt} />
-              <p>{product.content}</p>
-            </div>
-          ))}
-        </div>
+    <div className='product-container'>
+      <div className="product-image-gallery">
+        {products.map((product, index) => (
+          <div className="product-image-card" key={index}>
+            <img src={product.url} alt={product.alt} />
+            <p>{product.content}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
