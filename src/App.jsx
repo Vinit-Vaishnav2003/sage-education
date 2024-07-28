cf27827ad14530ff7f9d5da8d47013d98aa671fc
Vinit <vinitvaishnav2@gmail.com>
2024-07-28 21:08:57 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Modal from './components/Modal';
 import Client from './Pages/Client';
 import Product from './Pages/ProductProduct';
 import Footer from './components/Footer';
-
 import SecondPage from './Pages/SecondPage';
 
 
@@ -33,9 +32,6 @@ function App() {
       {isModalOpen && <Modal onClose={handleCloseModal} />}
       <div id="client" style={{ paddingTop: '70px', marginTop: '-70px' }}>
         <Client />
-      </div>
-      <div id="about" style={{ paddingTop: '70px', marginTop: '-70px' }}>
-        <About />
       </div>
       <div id="product" style={{ paddingTop: '70px', marginTop: '-70px' }}>
         <Product />
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,14 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <p>We are a youth-driven, career-education firm run by young people with the goal of providing students with
-simple, yet psychologically sound, career-counselling services with credible and legit information about
-careers, colleges, subjects, degrees, industries, and everything else matters in the process of career
-decision-making. With personal contact when working with 10th graders, we try to solve their every
-career-related worry. We discovered that only 7% of those from financially secure families succeed in
-obtaining proper career guidance. We have guided 1000+ students till now and are seeking to achieve an
-ultimate goal of reaching and helping every tier 2 and tier 3 city student in their
-journey to pursue their bright career.</p>
+      
     </div>
   )
 }
