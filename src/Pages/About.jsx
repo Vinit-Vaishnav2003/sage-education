82c111c70c34235f998da5f50a834bf81f6f005c
Aniruddha Pawar <aniruddha662003@gmail.com>
2024-08-03 23:53:53 +0530

    about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,7 +6,6 @@ function About() {
   return (
     <div className="about-container">
       <div className="about-content">
-       
         <div className="about-text">
           <h1>About Us</h1>
           <p>
