9c1be1f0786b6e448fa00e0f6ea032d519b017c0
Aniruddha Pawar <aniruddha662003@gmail.com>
2024-08-03 21:05:15 +0530

    some more changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './Home.css';
-
+import asto from '../assets/astronust/asto3.png'
 const Home = () => {
   return (
     <div className="home">
       <div className="container">
-        <br />
-        <h1 className="home-title animate-fade-in">Hello Pal!</h1>
-        <br />
-        <h2 className="home-subtitle my-3 animate-fade-in">Hope You are doing Great !</h2>
-        <h2 className="home-subtitle my-3 animate-fade-in">We are Sage Educations, we're passionate about guiding students toward their ideal careers.</h2>
-        <br />
-        <h2 className="home-text animate-fade-in">And yes, we've poured our money just to prove the fact that Sage Educations is indeed a genuine career counselling firm,</h2>
-        <h2 className="home-text animate-fade-in">For the past three years, we've been helping students in Mumbai navigate their career paths through our Personalised Career Reports and Counselling Sessions.</h2>
+        <div className="conttent"><br />
+          <div className="asto-container">
+            <h1 className="home-title animate-fade-in">Hello Pal!</h1>
+            <img src={asto} alt="" />
+          </div>
+          <br />
+          <h2 className="home-subtitle my-3 animate-fade-in">Hope You are doing Great !</h2>
+          <h2 className="home-subtitle my-3 animate-fade-in">We are Sage Educations, we're passionate about guiding students toward their ideal careers.</h2>
+          <br />
+          <h2 className="home-text animate-fade-in">And yes, we've poured our money just to prove the fact that Sage Educations is indeed a genuine career counselling firm,</h2>
+          <h2 className="home-text animate-fade-in">For the past three years, we've been helping students in Mumbai navigate their career paths through our Personalised Career Reports and Counselling Sessions.</h2>
+        </div>
       </div>
+
     </div>
   );
 }
